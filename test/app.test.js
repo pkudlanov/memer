ead7da295c7cb379eff1481eee50a7224c7da74c
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-16 14:53:43 -0700

    POST test to test meme getting created

--- test/app.test.js
+++ test/app.test.js
@@ -6,15 +6,34 @@ const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 
 describe('app routes', () => {
-  beforeAll(() => {
-    connect();
-  });
+	beforeAll(() => {
+		connect();
+	});
 
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
+	beforeEach(() => {
+		return mongoose.connection.dropDatabase();
+	});
 
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
+	afterAll(() => {
+		return mongoose.connection.close();
+	});
+
+	it('creates a meme with POST', () => {
+		return request(app)
+			.post('./api/v1/memes')
+			.send({
+				top: 'Those Dirty',
+				image: 'lib/assets/child-fist.jpg',
+				bottom: 'No. 2 Pencils'
+			})
+			.then(res => {
+				expect(res.body).toEqual({
+					_id: expect.any(String),
+					top: 'Those Dirty',
+					image: 'lib/assets/child-fist.jpg',
+					bottom: 'No. 2 Pencils',
+					__v: 0
+				});
+			});
+	});
 });
